15f4634d888239b318bf5ff4ef280120e8d53b46
DJGD0 <dgd0006@alu.medac.es>
2026-05-02 21:07:59 +0200

    Refactor: Fix Long Method in Main by extracting character initialization logic

--- Main.java
+++ Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		OnePiecePer[] Personaje=new OnePiecePer[4];
+		OnePiecePer[] Personaje = getOnePiecePers();
 
 		Scanner teclado =new Scanner (System.in);
 		
@@ -30,6 +30,11 @@ public class Main {
 			
 		}
 
+	}
+
+	private static OnePiecePer[] getOnePiecePers() {
+		OnePiecePer[] Personaje=new OnePiecePer[4];
+		return Personaje;
 	}
 
 }
