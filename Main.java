1ceb7399081c4b6b097d4c97146924de1bd53878
DJGD0 <dgd0006@alu.medac.es>
2026-05-02 22:19:13 +0200

    Refactor: Fix Temporary Fields by inlining local calculation variables

--- Main.java
+++ Main.java
@@ -4,37 +4,36 @@ import java.util.Scanner;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static OnePiecePer[] main(String[] args) {
 		OnePiecePer[] Personaje = getOnePiecePers();
 
-		Scanner teclado =new Scanner (System.in);
-		
-		OnePiecePer C1 =new OnePiecePer ("Monkey D. Luffy", 30000000, "Hito Hito no mi Modelo Nika", "Mugiwaras", "Capitán", true, "Humano");
-		OnePiecePer C2 =new OnePiecePer ("Kaido", 50000000, "Uo Uo no mi Modelo Dragón", "Piratas Bestia", "Capitán", true, "Oni");
-		OnePiecePer C3 =new OnePiecePer ("Buggy", 30000000, "Bara Bara no mi", "Cross Guild", "Capitán", false, "Humano");
-		OnePiecePer C4 =new OnePiecePer ("Foxy", 500000, "Noro Noro no mi", "Piratas de Foxy", "Capitán", false, "Humano");
-		
-		
-		
-		
-		Personaje[0]= C1;
-		Personaje[1]= C2;
-		Personaje[2]= C3;
-		Personaje[3]= C4;
-		
-		for(int i=0; i<Personaje.length; i++) 
-		{
-				System.out.println(Personaje[i]);
-				Personaje[i].esPeligroso();
-				System.out.println();
-			
+
+
+		OnePiecePer C1 = new OnePiecePer("Monkey D. Luffy", 30000000, "Hito Hito no mi Modelo Nika", "Mugiwaras", "Capitán", true, "Humano");
+		OnePiecePer C2 = new OnePiecePer("Kaido", 50000000, "Uo Uo no mi Modelo Dragón", "Piratas Bestia", "Capitán", true, "Oni");
+		OnePiecePer C3 = new OnePiecePer("Buggy", 30000000, "Bara Bara no mi", "Cross Guild", "Capitán", false, "Humano");
+		OnePiecePer C4 = new OnePiecePer("Foxy", 500000, "Noro Noro no mi", "Piratas de Foxy", "Capitán", false, "Humano");
+
+
+		Personaje[0] = C1;
+		Personaje[1] = C2;
+		Personaje[2] = C3;
+		Personaje[3] = C4;
+
+		for (int i = 0; i < Personaje.length; i++) {
+			System.out.println(Personaje[i]);
+			Personaje[i].esPeligroso();
+			System.out.println();
+
 		}
 
-	}
+
+        return Personaje;
+    }
 
 	private static OnePiecePer[] getOnePiecePers() {
-		OnePiecePer[] Personaje=new OnePiecePer[4];
+		OnePiecePer[] Personaje = new OnePiecePer[4];
+		Scanner teclado = new Scanner(System.in);
 		return Personaje;
 	}
-
 }
